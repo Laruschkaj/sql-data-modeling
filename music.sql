43de131c12c8ff5754594050a485295496cf861e
Laruschkaj <laruschkajoubert@gmail.com>
2025-08-07 17:49:14 -0400

    Initial commit for Part 2 of data modeling project

--- music.sql
+++ music.sql
@@ -1,33 +1,147 @@
+-- Refactored Music Schema with Data
 -- from the terminal run:
 -- psql < music.sql
 
 DROP DATABASE IF EXISTS music;
-
 CREATE DATABASE music;
-
 \c music
 
-CREATE TABLE songs
-(
-  id SERIAL PRIMARY KEY,
-  title TEXT NOT NULL,
-  duration_in_seconds INTEGER NOT NULL,
-  release_date DATE NOT NULL,
-  artists TEXT[] NOT NULL,
-  album TEXT NOT NULL,
-  producers TEXT[] NOT NULL
+-- Create artists table
+CREATE TABLE artists (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-INSERT INTO songs
-  (title, duration_in_seconds, release_date, artists, album, producers)
-VALUES
-  ('MMMBop', 238, '04-15-1997', '{"Hanson"}', 'Middle of Nowhere', '{"Dust Brothers", "Stephen Lironi"}'),
-  ('Bohemian Rhapsody', 355, '10-31-1975', '{"Queen"}', 'A Night at the Opera', '{"Roy Thomas Baker"}'),
-  ('One Sweet Day', 282, '11-14-1995', '{"Mariah Cary", "Boyz II Men"}', 'Daydream', '{"Walter Afanasieff"}'),
-  ('Shallow', 216, '09-27-2018', '{"Lady Gaga", "Bradley Cooper"}', 'A Star Is Born', '{"Benjamin Rice"}'),
-  ('How You Remind Me', 223, '08-21-2001', '{"Nickelback"}', 'Silver Side Up', '{"Rick Parashar"}'),
-  ('New York State of Mind', 276, '10-20-2009', '{"Jay Z", "Alicia Keys"}', 'The Blueprint 3', '{"Al Shux"}'),
-  ('Dark Horse', 215, '12-17-2013', '{"Katy Perry", "Juicy J"}', 'Prism', '{"Max Martin", "Cirkut"}'),
-  ('Moves Like Jagger', 201, '06-21-2011', '{"Maroon 5", "Christina Aguilera"}', 'Hands All Over', '{"Shellback", "Benny Blanco"}'),
-  ('Complicated', 244, '05-14-2002', '{"Avril Lavigne"}', 'Let Go', '{"The Matrix"}'),
-  ('Say My Name', 240, '11-07-1999', '{"Destiny''s Child"}', 'The Writing''s on the Wall', '{"Darkchild"}');
+-- Create producers table
+CREATE TABLE producers (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
+);
+
+-- Create albums table
+CREATE TABLE albums (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    release_date DATE NOT NULL
+);
+
+-- Create songs table and link to albums
+CREATE TABLE songs (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    duration_in_seconds INTEGER NOT NULL,
+    album_id INTEGER REFERENCES albums(id)
+);
+
+-- Join table for songs and artists (many-to-many)
+CREATE TABLE song_artists (
+    song_id INTEGER REFERENCES songs(id),
+    artist_id INTEGER REFERENCES artists(id),
+    PRIMARY KEY (song_id, artist_id)
+);
+
+-- Join table for songs and producers (many-to-many)
+CREATE TABLE song_producers (
+    song_id INTEGER REFERENCES songs(id),
+    producer_id INTEGER REFERENCES producers(id),
+    PRIMARY KEY (song_id, producer_id)
+);
+
+-- Insert data into parent tables
+INSERT INTO artists (name) VALUES
+    ('Hanson'),
+    ('Queen'),
+    ('Mariah Cary'),
+    ('Boyz II Men'),
+    ('Lady Gaga'),
+    ('Bradley Cooper'),
+    ('Nickelback'),
+    ('Jay Z'),
+    ('Alicia Keys'),
+    ('Katy Perry'),
+    ('Juicy J'),
+    ('Maroon 5'),
+    ('Christina Aguilera'),
+    ('Avril Lavigne'),
+    ('Destiny''s Child');
+
+INSERT INTO producers (name) VALUES
+    ('Dust Brothers'),
+    ('Stephen Lironi'),
+    ('Roy Thomas Baker'),
+    ('Walter Afanasieff'),
+    ('Benjamin Rice'),
+    ('Rick Parashar'),
+    ('Al Shux'),
+    ('Max Martin'),
+    ('Cirkut'),
+    ('Shellback'),
+    ('Benny Blanco'),
+    ('The Matrix'),
+    ('Darkchild');
+
+INSERT INTO albums (name, release_date) VALUES
+    ('Middle of Nowhere', '1997-04-15'),
+    ('A Night at the Opera', '1975-10-31'),
+    ('Daydream', '1995-11-14'),
+    ('A Star Is Born', '2018-09-27'),
+    ('Silver Side Up', '2001-08-21'),
+    ('The Blueprint 3', '2009-10-20'),
+    ('Prism', '2013-12-17'),
+    ('Hands All Over', '2011-06-21'),
+    ('Let Go', '2002-05-14'),
+    ('The Writing''s on the Wall', '1999-11-07');
+
+-- Insert song data referencing albums
+INSERT INTO songs (title, duration_in_seconds, album_id) VALUES
+    ('MMMBop', 238, 1),
+    ('Bohemian Rhapsody', 355, 2),
+    ('One Sweet Day', 282, 3),
+    ('Shallow', 216, 4),
+    ('How You Remind Me', 223, 5),
+    ('New York State of Mind', 276, 6),
+    ('Dark Horse', 215, 7),
+    ('Moves Like Jagger', 201, 8),
+    ('Complicated', 244, 9),
+    ('Say My Name', 240, 10);
+
+-- Insert data into join tables for many-to-many relationships
+-- Song 1: MMMBop
+INSERT INTO song_artists (song_id, artist_id) VALUES (1, 1);
+INSERT INTO song_producers (song_id, producer_id) VALUES (1, 1), (1, 2);
+
+-- Song 2: Bohemian Rhapsody
+INSERT INTO song_artists (song_id, artist_id) VALUES (2, 2);
+INSERT INTO song_producers (song_id, producer_id) VALUES (2, 3);
+
+-- Song 3: One Sweet Day
+INSERT INTO song_artists (song_id, artist_id) VALUES (3, 3), (3, 4);
+INSERT INTO song_producers (song_id, producer_id) VALUES (3, 4);
+
+-- Song 4: Shallow
+INSERT INTO song_artists (song_id, artist_id) VALUES (4, 5), (4, 6);
+INSERT INTO song_producers (song_id, producer_id) VALUES (4, 5);
+
+-- Song 5: How You Remind Me
+INSERT INTO song_artists (song_id, artist_id) VALUES (5, 7);
+INSERT INTO song_producers (song_id, producer_id) VALUES (5, 6);
+
+-- Song 6: New York State of Mind
+INSERT INTO song_artists (song_id, artist_id) VALUES (6, 8), (6, 9);
+INSERT INTO song_producers (song_id, producer_id) VALUES (6, 7);
+
+-- Song 7: Dark Horse
+INSERT INTO song_artists (song_id, artist_id) VALUES (7, 10), (7, 11);
+INSERT INTO song_producers (song_id, producer_id) VALUES (7, 8), (7, 9);
+
+-- Song 8: Moves Like Jagger
+INSERT INTO song_artists (song_id, artist_id) VALUES (8, 12), (8, 13);
+INSERT INTO song_producers (song_id, producer_id) VALUES (8, 10), (8, 11);
+
+-- Song 9: Complicated
+INSERT INTO song_artists (song_id, artist_id) VALUES (9, 14);
+INSERT INTO song_producers (song_id, producer_id) VALUES (9, 12);
+
+-- Song 10: Say My Name
+INSERT INTO song_artists (song_id, artist_id) VALUES (10, 15);
+INSERT INTO song_producers (song_id, producer_id) VALUES (10, 13);
--- outer_space.sql
+++ outer_space.sql
@@ -1,28 +1,70 @@
+-- Refactored Outer Space Schema with Data
 -- from the terminal run:
 -- psql < outer_space.sql
 
 DROP DATABASE IF EXISTS outer_space;
-
 CREATE DATABASE outer_space;
-
 \c outer_space
 
-CREATE TABLE planets
-(
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
-  orbits_around TEXT NOT NULL,
-  galaxy TEXT NOT NULL,
-  moons TEXT[]
+-- Create galaxies table
+CREATE TABLE galaxies (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL
 );
 
-INSERT INTO planets
-  (name, orbital_period_in_years, orbits_around, galaxy, moons)
-VALUES
-  ('Earth', 1.00, 'The Sun', 'Milky Way', '{"The Moon"}'),
-  ('Mars', 1.88, 'The Sun', 'Milky Way', '{"Phobos", "Deimos"}'),
-  ('Venus', 0.62, 'The Sun', 'Milky Way', '{}'),
-  ('Neptune', 164.8, 'The Sun', 'Milky Way', '{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
-  ('Proxima Centauri b', 0.03, 'Proxima Centauri', 'Milky Way', '{}'),
-  ('Gliese 876 b', 0.23, 'Gliese 876', 'Milky Way', '{}');
+-- Create stars table and link to galaxies
+CREATE TABLE stars (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    galaxy_id INTEGER REFERENCES galaxies(id)
+);
+
+-- Create planets table and link to stars
+CREATE TABLE planets (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    orbital_period_in_years FLOAT NOT NULL,
+    star_id INTEGER REFERENCES stars(id)
+);
+
+-- Create moons table and link to planets
+CREATE TABLE moons (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    planet_id INTEGER REFERENCES planets(id)
+);
+
+-- Insert data into the galaxies table first
+INSERT INTO galaxies (name) VALUES ('Milky Way');
+
+-- Insert data into the stars table, referencing the galaxy
+INSERT INTO stars (name, galaxy_id) VALUES ('The Sun', 1), ('Proxima Centauri', 1), ('Gliese 876', 1);
+
+-- Insert data into the planets table, referencing the stars
+INSERT INTO planets (name, orbital_period_in_years, star_id) VALUES
+    ('Earth', 1.00, 1),
+    ('Mars', 1.88, 1),
+    ('Venus', 0.62, 1),
+    ('Neptune', 164.8, 1),
+    ('Proxima Centauri b', 0.03, 2),
+    ('Gliese 876 b', 0.23, 3);
+
+-- Insert data into the moons table, referencing the planets
+INSERT INTO moons (name, planet_id) VALUES
+    ('The Moon', 1),
+    ('Phobos', 2),
+    ('Deimos', 2),
+    ('Naiad', 4),
+    ('Thalassa', 4),
+    ('Despina', 4),
+    ('Galatea', 4),
+    ('Larissa', 4),
+    ('S/2004 N 1', 4),
+    ('Proteus', 4),
+    ('Triton', 4),
+    ('Nereid', 4),
+    ('Halimede', 4),
+    ('Sao', 4),
+    ('Laomedeia', 4),
+    ('Psamathe', 4),
+    ('Neso', 4);
